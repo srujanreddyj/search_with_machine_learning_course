98d7d67469db9bce5d8d9cf53c77b56aaca01132
srujanreddyj <jsrujanreddy@gmail.com>
2022-02-18 10:12:40 -0600

    Update opensearch.py

--- week1/opensearch.py
+++ week1/opensearch.py
@@ -1,153 +1,22 @@
-#
-# The main search hooks for the Search Flask application.
-#
-from flask import (
-    Blueprint, redirect, render_template, request, url_for
-)
+from flask import g, current_app
+from opensearchpy import OpenSearch
 
-from week1.opensearch import get_opensearch
+# Create an OpenSearch client instance and put it into Flask shared space for use by the application
+def get_opensearch():
+    if 'opensearch' not in g:
+        # Implement a client connection to OpenSearch so that the rest of the application can communicate with OpenSearch
+        g.opensearch = None
+        host = 'localhost'
+        port = 9200
 
-bp = Blueprint('search', __name__, url_prefix='/search')
+        g.opensearch = OpenSearch(
+            hosts=[{'host': host, 'port':port}]
+            http_compress=True,
+            http_auth=('admin', 'admin'),
+            use_ssl=True,
+            verify_certs=False,
+            ssl_assert_hostname=False,
+            ssl_show_warn=False
+        )
 
-
-# Process the filters requested by the user and return a tuple that is appropriate for use in: the query, URLs displaying the filter and the display of the applied filters
-# filters -- convert the URL GET structure into an OpenSearch filter query
-# display_filters -- return an array of filters that are applied that is appropriate for display
-# applied_filters -- return a String that is appropriate for inclusion in a URL as part of a query string.  This is basically the same as the input query string
-def process_filters(filters_input):
-    # Filters look like: &filter.name=regularPrice&regularPrice.key={{ agg.key }}&regularPrice.from={{ agg.from }}&regularPrice.to={{ agg.to }}
-    filters = []
-    display_filters = []  # Also create the text we will use to display the filters that are applied
-    applied_filters = ""
-    for filter in filters_input:
-        type = request.args.get(filter + ".type")
-        display_name = request.args.get(filter + ".displayName", filter)
-        #
-        # We need to capture and return what filters are already applied so they can be automatically added to any existing links we display in aggregations.jinja2
-        applied_filters += "&filter.name={}&{}.type={}&{}.displayName={}".format(filter, filter, type, filter,
-                                                                                 display_name)
-        #TODO: IMPLEMENT AND SET filters, display_filters and applied_filters.
-        display_filters.append(display_name)
-        # filters get used in create_query below.  display_filters gets used by display_filters.jinja2 and applied_filters gets used by aggregations.jinja2 (and any other links that would execute a search.)
-        if type == "range":
-            pass
-        elif type == "terms":
-            pass #TODO: IMPLEMENT
-    print("Filters: {}".format(filters))
-
-    return filters, display_filters, applied_filters
-
-
-# Our main query route.  Accepts POST (via the Search box) and GETs via the clicks on aggregations/facets
-@bp.route('/query', methods=['GET', 'POST'])
-def query():
-    opensearch = get_opensearch() # Load up our OpenSearch client from the opensearch.py file.
-    # Put in your code to query opensearch.  Set error as appropriate.
-    error = None
-    user_query = None
-    query_obj = None
-    display_filters = None
-    applied_filters = ""
-    filters = None
-    sort = "_score"
-    sortDir = "desc"
-    if request.method == 'POST':  # a query has been submitted
-        user_query = request.form['query']
-        if not user_query:
-            user_query = "*"
-        sort = request.form["sort"]
-        if not sort:
-            sort = "_score"
-        sortDir = request.form["sortDir"]
-        if not sortDir:
-            sortDir = "desc"
-        query_obj = create_query(user_query, [], sort, sortDir)
-    elif request.method == 'GET':  # Handle the case where there is no query or just loading the page
-        user_query = request.args.get("query", "*")
-        filters_input = request.args.getlist("filter.name")
-        sort = request.args.get("sort", sort)
-        sortDir = request.args.get("sortDir", sortDir)
-        if filters_input:
-            (filters, display_filters, applied_filters) = process_filters(filters_input)
-
-        query_obj = create_query(user_query, filters, sort, sortDir)
-    else:
-        query_obj = create_query("*", [], sort, sortDir)
-
-    print("query obj: {}".format(query_obj))
-    #response = None  ## TODO: Replace me with an appropriate call to OpenSearch
-    response = opensearch.search(body=query_obj, index='bbuy_products', doc_type="_doc")
-    # Postprocess results here if you so desire
-
-    #print(response)
-    if error is None:
-        return render_template("search_results.jinja2", query=user_query, search_response=response,
-                               display_filters=display_filters, applied_filters=applied_filters,
-                               sort=sort, sortDir=sortDir)
-    else:
-        redirect(url_for("index"))
-
-
-def create_query(user_query, filters, sort="_score", sortDir="desc"):
-    print("Query: {} Filters: {} Sort: {}".format(user_query, filters, sort))
-    compQuery = [{
-        "multi_match":{
-            "fields": ['name', "shortDescription", "longDescription", "department", 'manufacturer'],
-            "query": user_query,
-            "type": "most_fields"
-        }
-    },
-    {
-        "multi_match":{
-            "fields": ['name^50', "shortDescription^50", "longDescription^50", "department"],
-            "query": user_query,
-            "type": "phrase"
-        }
-    },
-    {
-        "match":{
-            "name.word_bigram":{
-                "query": user_query,
-                "boost": 2000
-            }
-        }
-    }
-    ]
-    query_obj = {
-        'size': 10,
-        "query": {
-            "bool":{
-                "should":compQuery,
-                "minimum_should_match":1
-            }
-        },
-        # "query": {
-        #     "match_all": {} # Replace me with a query that both searches and filters
-        # },
-        "aggs": {
-            "regularPrice":{
-                "range":{
-                    "field": "regularPrice",
-                    "ranges":  [
-                        {
-                            "from":0,
-                            "to":250
-                        },
-                        {
-                            "from":250,
-                            "to":1000,
-                        },
-                        {
-                            "from":1000
-                        }
-                    ]
-                },
-                "missing_images":{
-                    "missing":{
-                        "field": "image"
-                    }
-                }
-            }
-        }
-    }
-    return query_obj
+    return g.opensearch
